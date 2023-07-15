ff0ebc16542b2df9caf1c58c2c7c3c2f65d93148
Gilgameshgb-1 <farisbazdar1@gmail.com>
2023-07-15 14:08:23 +0200

    Fixed visualisation, placeholder for brand logo/image

--- public/GenerateQR/main.js
+++ public/GenerateQR/main.js
@@ -1,5 +1,6 @@
 let btn = document.querySelector(".button");
-let qr_code_element = document.querySelector(".qr-code");
+let qr_code_element = document.querySelector(".qr-code-png");
+let qr_proprietary = document.querySelector(".qr-code");
 
 document.addEventListener("DOMContentLoaded", () => {
   //console.log(document.querySelector("#input_text").value);
@@ -41,16 +42,24 @@ function generate(user_input) {
     correctLevel: QRCode.CorrectLevel.H,
   });
 
+  let brandLogo = document.createElement("img");
+  brandLogo.src = "brand-logo.png";
+  brandLogo.alt = "Brand Logo";
+  brandLogo.classList.add("brand-logo");
+
+  qr_proprietary.appendChild(brandLogo);
+
   let download = document.createElement("button");
-  qr_code_element.appendChild(download);
+  qr_proprietary.appendChild(download);
 
   let download_link = document.createElement("a");
-  download_link.setAttribute("download", "qr_code.png");
-  download_link.innerHTML = `Download <i class="fa-solid fa-download"></i>`;
-
   download.appendChild(download_link);
+  download.style.marginBottom = "10px";
+  console.log(download_link);
+  download_link.setAttribute("download", "qr_code.png");
+  download_link.innerHTML = `Download QR code <i class="fa-solid fa-download"></i>`;
 
-  let qr_code_img = document.querySelector(".qr-code img");
+  let qr_code_img = document.querySelector(".qr-code-png");
   let qr_code_canvas = document.querySelector("canvas");
 
   if (qr_code_img.getAttribute("src") == null) {
@@ -62,4 +71,35 @@ function generate(user_input) {
       download_link.setAttribute("href", `${qr_code_img.getAttribute("src")}`);
     }, 300);
   }
+
+  let downloadPdfButton = document.createElement("button");
+  downloadPdfButton.innerHTML = `Download PDF <i class="fa-solid fa-download"></i>`;
+  qr_proprietary.appendChild(downloadPdfButton);
+
+  downloadPdfButton.addEventListener("click", function () {
+    let pdf = new jsPDF();
+    pdf.text("QR Code", 10, 10);
+    if (qr_code_img.getAttribute("src") == null) {
+      pdf.addImage(qr_code_canvas.toDataURL(), "PNG", 10, 20, 180, 180);
+    } else {
+      pdf.addImage(qr_code_img.getAttribute("src"), "PNG", 10, 20, 180, 180);
+    }
+    pdf.save("qr_code.pdf");
+  });
 }
+
+/* document.getElementById("downloadBtn").addEventListener("click", function () {
+  // Retrieve the ticket data
+  const ticketData = JSON.stringify(localStorage.getItem("formDataObject"));
+
+  // Create a new jsPDF instance
+  const doc = new jsPDF();
+
+  // Set the content of the PDF
+  doc.text("Ticket Data:", 10, 10);
+  doc.text(ticketData, 10, 20);
+
+  // Save the PDF as a file
+  doc.save("ticket.pdf");
+});
+ */
